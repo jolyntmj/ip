d0b765580c634e0d119df76166597db3bfb8d3d0
jolyntmj <jolyn.toh@u.nus.edu>
2026-02-20 23:39:17 +0800

    Fix gradlew build fail

--- src/main/java/sealriously/ui/MainWindow.java
+++ src/main/java/sealriously/ui/MainWindow.java
@@ -1,5 +1,6 @@
 package sealriously.ui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -7,7 +8,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.application.Platform;
 import sealriously.Sealriously;
 
 /**
